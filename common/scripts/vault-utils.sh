f1dda7bca1d6ba7f668a1f027fd1b20b348fc74a
Manuel Lorenzo <mlorenzofr@redhat.com>
2025-08-27 23:07:14 +0200

    mbp-935: Configure Vault to support JWT authentication
    Signed-off-by: Manuel Lorenzo <mlorenzofr@redhat.com>

--- common/scripts/vault-utils.sh
+++ common/scripts/vault-utils.sh
@@ -27,4 +27,25 @@ fi
 
 EXTRA_PLAYBOOK_OPTS="${EXTRA_PLAYBOOK_OPTS:-}"
 
-ansible-playbook -t "${TASK}" -e pattern_name="${PATTERN_NAME}" -e pattern_dir="${PATTERNPATH}" ${EXTRA_PLAYBOOK_OPTS} "rhvp.cluster_utils.vault"
+if [ "$(yq ".clusterGroup.applications.vault.jwt.enabled" values-hub.yaml)" == "true" ]; then
+  OCP_DOMAIN="$(oc get dns cluster -o jsonpath='{.spec.baseDomain}')"
+  OIDC_DISCOVERY_URL="$(yq ".clusterGroup.applications.vault.jwt.oidcDiscoveryUrl" values-hub.yaml | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
+  DEFAULT_ROLE="$(yq ".clusterGroup.applications.vault.jwt.roles[0].name" values-hub.yaml)"
+  SPIFFE_AUDIENCE="$(yq ".clusterGroup.applications.vault.jwt.roles[0].audience" values-hub.yaml)"
+  SPIFFE_SUBJECT="$(yq ".clusterGroup.applications.vault.jwt.roles[0].subject" values-hub.yaml | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
+  VAULT_JWT_CONFIG="true"
+  echo "Vault JWT config is enabled"
+else
+  VAULT_JWT_CONFIG="false"
+  echo "Vault JWT config is disabled"
+fi
+
+ansible-playbook -t "${TASK}" \
+  -e pattern_name="${PATTERN_NAME}" \
+  -e pattern_dir="${PATTERNPATH}" \
+  -e vault_jwt_config="${VAULT_JWT_CONFIG}" \
+  -e oidc_discovery_url="${OIDC_DISCOVERY_URL:-}" \
+  -e default_role="${DEFAULT_ROLE:-}" \
+  -e spiffe_audience="${SPIFFE_AUDIENCE:-}" \
+  -e spiffe_subject="${SPIFFE_SUBJECT:-}" \
+  ${EXTRA_PLAYBOOK_OPTS} "rhvp.cluster_utils.vault"
